2d39c55a1449fbf22003dbde323ce44dca2dbfbe
amitoj <amitojsh@hotmail.com>
2018-04-20 17:20:41 +0100

    v1.4.0

---
+++ cf-app-4/src/main/java/com/hanspal/cfapp4/CfApp4Application.java
@@ -0,0 +1,12 @@
+package com.hanspal.cfapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfApp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfApp4Application.class, args);
+    }
+}
