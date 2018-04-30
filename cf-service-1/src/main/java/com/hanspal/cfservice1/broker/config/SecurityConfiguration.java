434b0b19616ad84aea1b881d87ed566ca6c0df41
amitoj <amitojsh@hotmail.com>
2018-04-30 15:55:45 +0100

    v1.5.0_1

--- cf-service-1/src/main/java/com/hanspal/cfservice1/broker/config/SecurityConfiguration.java
+++ cf-service-1/src/main/java/com/hanspal/cfservice1/broker/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.hanspal.cfservice1.broker.config;
 
 import com.hanspal.cfservice1.service.web.security.SecurityAuthorities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        log.info("SecurityConfiguration: configure ");
+
         // @formatter:off
         http
                 .csrf().disable()
