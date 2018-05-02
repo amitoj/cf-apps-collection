bdb6c3c3472731c3da315ccb0f8349e32c89a347
amitojsh@hotmail.com <@hanspal$9>
2018-05-02 10:06:06 +0100

    v1.5.0 - b

--- cf-service-1/src/main/java/com/hanspal/cfservice1/service/web/security/WebSecurityConfig.java
+++ cf-service-1/src/main/java/com/hanspal/cfservice1/service/web/security/WebSecurityConfig.java
@@ -2,12 +2,16 @@ package com.hanspal.cfservice1.service.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(1000)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired(required = false)
